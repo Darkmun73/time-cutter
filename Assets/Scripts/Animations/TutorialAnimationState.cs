2a5fd90b1eb3e4da1f0dd9ad91fe961fbbe6d67c
Darkmun73 <90666550+Darkmun73@users.noreply.github.com>
2026-05-27 13:49:09 +1000

    Replace animator with video player for tutorial videos

--- Assets/Scripts/Animations/TutorialAnimationState.cs
+++
@@ -1,6 +0,0 @@
-public class TutorialAnimationState : AnimationState
-{
-    public static readonly TutorialAnimationState Empty = new("Empty");
-
-    public TutorialAnimationState(string stateName) : base(stateName) {}
-}
